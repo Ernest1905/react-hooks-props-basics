fcfeb4790cc713bc3332da9dedddc5286f00b75d
Ernest <ernest.okiror@student.moringaschool.com>
2023-04-28 16:13:34 +0300

    Completed Assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+
+  function BlogContent(props) {
+    console.log(props);
+  
+    if (!props.isPublished) {
+      // hide unpublished content
+      // return null means "don't display any DOM elements here"
+      return null;
+    } else {
+      // show published content
+      return (
+        <div>
+          <h2>{props.articleText}</h2>
+          <p>{props.minutesToRead} minutes to read</p>
+          <p>{props.commentText}</p>
+        </div>
+      );
+    }
 }
 
 export default BlogContent;
